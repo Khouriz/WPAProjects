4cf2beb3487f61e8de760489252c6bcf3256ae3c
Khouriz <andrewz2473@gmail.com>
2021-06-02 18:37:54 -0700

    initial file commit

---
+++ PLCtoJSON.py
@@ -0,0 +1,40 @@
+##############################################################################
+#This project is going to be used by Wonderful: Pistachios and Almonds to
+#for R&D purposes. It is not to be cloned unless by authorized parties.
+#This is to test python capabilities to communicate to PLC through an OCP-UA
+#Protocol. Then converting them to a .json format and outputting the data
+#using an MQTT Protocol to communicate with AWS
+#Author: Andrew Zulfa
+#Company:Wonderful:Pistachios and Almonds
+#Supervisor:Graysen Rowland
+#StartDate:06/02/2021
+#EndDate:
+##############################################################################
+
+#libraries needed(subject to change)
+import time
+import calendar
+import socket
+import json
+#import {} (Need to find a library that can be used to communicate through an OCP-UA)
+
+def main():
+    #define variable#
+
+    #####################
+    
+    #initial communication#
+
+    #####################
+    
+    #initial comunication to json format#
+
+    #####################
+
+    #deletion and output of json file to better save space on the the system that is being run#
+
+    #####################
+
+    print("This is development and will be ready when it is!-Andrew")
+    exit
+main()
