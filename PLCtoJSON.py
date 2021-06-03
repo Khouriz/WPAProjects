e5bb0a4a3599c7f64343f1f3c4e9127b4fa88e61
Andrew Zulfa <andrewz2473@gmail.com>
2021-06-03 12:05:03 -0700

    initial attempts to communicate over an OPC-UA Protocol. For further dev I am going to need access to a PLC to further this segment

--- PLCtoJSON.py
+++ PLCtoJSON.py
@@ -16,15 +16,26 @@ import time
 import calendar
 import socket
 import json
-#import {} (Need to find a library that can be used to communicate through an OCP-UA)
+import sys
+from opcua import Client
 
 def main():
     #define variable#
-
+    client = Client("opc.tcp://#insert ip address here#")#may have to be redundant with anaonymous authentification if it comes to it
     #####################
-    
+    print(client.application_uri)
     #initial communication#
-
+    try:
+        client.connect();
+        TagName1 = client.get_node('ns=3;s="storageTempratures"."Temperature1C"')
+        TagName2 = client.get_node('#This is for an input similar above to access the info on the client side#')
+        print(var.get_data_value())
+        Value1 = float(TagName1.get_value())
+        Value2 = float(TagName2.get_value())
+        print("value 1 = " + str(Value1))
+        print("value 2 = " + str(Value2))
+    finally:
+        client.disconnect()
     #####################
     
     #initial comunication to json format#
